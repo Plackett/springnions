5058e12fbdfece2c826f305e4e28647bd28465e7
Plackett <johnnicholasdimitri@gmail.com>
2022-06-23 12:20:25 -0400

    onion_shelf now works!

--- src/main/java/com/blueking6/springnions/init/OnionShelf.java
+++ src/main/java/com/blueking6/springnions/init/OnionShelf.java
@@ -26,71 +26,70 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 public class OnionShelf extends Block implements EntityBlock {
 	public static final IntegerProperty HASITEM = IntegerProperty.create("has_item", 0, 8);
 	private static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-	private static final VoxelShape SHAPE =  Block.box(0, 0, 0, 16, 32, 16);
-	
+	private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 32, 16);
+
 	public OnionShelf(Block.Properties properties) {
 		super(properties);
 	}
 
 	@Nullable
-    @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return TileEntityInit.ONION_SHELF.get().create(pos, state);
-    }
-    
-    @Override
-	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player,
-			InteractionHand hand, BlockHitResult result) {
-    	 if (level.isClientSide && level.getBlockEntity(pos) instanceof final OnionShelfTile shelf) {
-    		 if(player.getItemInHand(hand).is(ItemInit.ONION_CRATE.get())) {
-    			 player.setItemInHand(hand, shelf.appendItem(player.getItemInHand(hand)));
-    			 state = UpdateState(level, pos, state, shelf);
-    			 level.setBlockAndUpdate(pos, state);
-    		 } else {
-    			 if(player.getItemInHand(hand).isEmpty()) {
-    				 player.setItemInHand(hand, shelf.returnItem());
-        			 state = UpdateState(level, pos, state, shelf);
-        			 level.setBlockAndUpdate(pos, state);
-    			 }
-    		 }
-             return InteractionResult.SUCCESS;
-          } else {
-             return InteractionResult.CONSUME;
-          }
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return TileEntityInit.ONION_SHELF.get().create(pos, state);
 	}
-    
-    public BlockState UpdateState(Level level, BlockPos pos, BlockState state, OnionShelfTile shelf) {
-    	int v = 0;
-    	for(int i = 0; i < 7; i++) {
-			 if(shelf.inventory.getStackInSlot(i).isEmpty() == false) {
-				 v++;
-			 }
-		 }
-    	return state.setValue(HASITEM, v);
-    }
-    
+
+	@Override
+	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand,
+			BlockHitResult result) {
+		if (level.isClientSide) {
+			return InteractionResult.SUCCESS;
+		} else {
+			if (!level.isClientSide && level.getBlockEntity(pos) instanceof final OnionShelfTile shelf) {
+				if (player.getItemInHand(hand).is(ItemInit.ONION_CRATE.get())) {
+					player.setItemInHand(hand, shelf.appendItem(player.getItemInHand(hand)));
+				} else {
+					if (player.getItemInHand(hand).isEmpty()) {
+						if (player.isCrouching()) {
+							for (int i = 0; i < shelf.getNumberOfItems(level, pos, shelf); i++) {
+								player.getInventory().add(shelf.returnItem());
+							}
+						} else {
+							player.getInventory().add(shelf.returnItem());
+						}
+					}
+				}
+				state = state.setValue(HASITEM, shelf.getNumberOfItems(level, pos, shelf));
+				level.setBlockAndUpdate(pos, state);
+				return InteractionResult.CONSUME;
+			}
+			return InteractionResult.CONSUME;
+		}
+	}
+
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
 		return SHAPE;
 	}
-	
+
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
 		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
 	}
-	
+
 	@Override
 	public BlockState rotate(BlockState state, Rotation rot) {
 		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@Override
 	public BlockState mirror(BlockState state, Mirror mirrorIn) {
 		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
 	}
-	
+
 	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) { builder.add(FACING, HASITEM); }
-	
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING, HASITEM);
+	}
+
 }
