58131f2be220dd9ccf99fe6d04b62864c75e89a7
Plackett <johnnicholasdimitri@gmail.com>
2023-07-04 18:26:13 -0400

    v2.0.1

--- src/main/java/com/blueking6/springnions/effects/EyePain.java
+++ src/main/java/com/blueking6/springnions/effects/EyePain.java
@@ -1,3 +1,25 @@
+//MIT License
+//
+//Copyright (c) 2023 Blueking6
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 package com.blueking6.springnions.effects;
 
 import net.minecraft.client.Minecraft;
@@ -26,7 +48,7 @@ public class EyePain extends MobEffect {
 
 	@Override
 	public void applyEffectTick(LivingEntity entity, int amp) {
-		if (shaderEnabled == false && entity.level().isClientSide == true) {
+		if (EyePain.shaderEnabled == false && entity.level().isClientSide == true) {
 			applyShader(new ResourceLocation("springnions:eye_pain"));
 		}
 		super.applyEffectTick(entity, amp);
@@ -37,14 +59,14 @@ public class EyePain extends MobEffect {
 		if (Minecraft.getInstance().gameRenderer.currentEffect() == null) {
 			ResourceLocation shader = location.withPrefix("shaders/post/").withSuffix(".json");
 			Minecraft.getInstance().gameRenderer.loadEffect(shader);
-			shaderEnabled = true;
+			EyePain.shaderEnabled = true;
 		}
 	}
 
 	@SuppressWarnings("resource")
 	public static void removeShaders() {
 		Minecraft.getInstance().gameRenderer.shutdownEffect();
-		shaderEnabled = false;
+		EyePain.shaderEnabled = false;
 	}
 
 	@Override
